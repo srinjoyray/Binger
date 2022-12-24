6ce8c162e2271ff699ab0562fe6589514753bb6f
Srinjoy Ray <srinjoyray@yahoo.in>
2022-12-25 01:02:00 +0530

    deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // const API = axios.create({baseURL:'http://localhost:5000'});
-
-const API = axios.create({baseURL:'https://binger1.herokuapp.com'});
+// const API = axios.create({baseURL:'https://binger1.herokuapp.com'});
+const API = axios.create({baseURL:'https://binger-ec26.onrender.com'});
 
 
 API.interceptors.request.use((req)=>{
